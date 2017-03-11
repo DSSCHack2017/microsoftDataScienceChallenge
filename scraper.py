313ddbe2b20e62fede1a93c0bfde31740847c30d
abdulfaizp <abdulfaizp@gmail.com>
2017-03-11 23:24:36 +0000

    initial file

--- scraper.py
+++ scraper.py
@@ -1,7 +1,36 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = "https://www.bloomberg.com/markets/earnings-calendar/us"
 
+tickers = ['JP', 'KS', 'CH', 'HK', 'AAPL']
+
+
+def get_news(link):
+    base_link = "http://www.reuters.com/"
+    link = base_link + link
+    r = requests.get(link)
+
+    article_soup = BeautifulSoup(r.content, 'html')
+
+    article_span = soup.findAll('span', {'id': 'article-text'})
+
+    paras = article_span[0].findAll('p')
+
+    news = ' '.join(list(map(lambda k: k.getText(), paras)))
+
+    return news
+
+# date DDMMYYYY
 def get_data_for_date(ticker, date):
-    
+    url = "http://www.reuters.com/finance/stocks/companyNews?symbol={}.O&date={}".format(ticker, date)
+    r = requests.get(url)
+    soup = BeautifulSoup(requests.get(url), 'html')
+    links = soup.findAll('a')
+
+    links_to_articles = list(map(lambda k: k[:8], links))
+
+    day_news = ' '.join(list(map(lambda link: get_news(link), links_to_articles)))
+
+    return day_news
+
+def main():
