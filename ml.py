ed0213c0b4311c8e8069575e65c77a0fa09bf844
Anirudh <anirudhx5@gmail.com>
2017-03-12 07:32:11 +0000

    magic data

--- ml.py
+++ ml.py
@@ -1,6 +1,7 @@
 from get_stock_data import *
 import json
 import pandas as pd
+import matplotlib.pyplot as plt
 import datetime
 
 
@@ -17,7 +18,6 @@ def create_df_for_ticker(ticker):
     tickers = [ticker]
     dates = pd.date_range('2012-01-01', '2012-12-31')
     df = get_data(tickers, dates)
-    df = compute_daily_returns(df)
     dates = pd.date_range('2013-01-01', '2013-12-31')
     df = df.append(get_data(tickers, dates))
     dates = pd.date_range('2014-01-01', '2014-12-31')
@@ -26,6 +26,8 @@ def create_df_for_ticker(ticker):
     df = df.append(get_data(tickers, dates))
     dates = pd.date_range('2016-01-01', '2016-12-31')
     df = df.append(get_data(tickers, dates))
+
+    df = compute_daily_returns(df)
 
     with open('data/{}.json'.format(ticker)) as json_data:
         data = json.load(json_data)
@@ -42,5 +44,12 @@ def create_df_for_ticker(ticker):
 
     return cd
 
-cd = create_df_for_ticker('AAPL')
-print(cd)
+aapl = create_df_for_ticker('AAPL')
+ax = aapl.plot()
+plt.show()
+# goog = create_df_for_ticker('GOOG')
+# msft = create_df_for_ticker('MSFT')
+
+# print(aapl)
+# print(goog)
+# print(msft)
