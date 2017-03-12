ce5364e8f9ae33decac9c0bcc51079f59e9252df
Anirudh <anirudhx5@gmail.com>
2017-03-12 07:15:32 +0000

    cleaned code

--- ml.py
+++ ml.py
@@ -5,24 +5,7 @@ import datetime
 
 
 # tickers = ['AAPL', 'MSFT', 'GOOG']
-tickers = ['AAPL']
-dates = pd.date_range('2012-01-01', '2012-12-31')
-df = get_data(tickers, dates)
-df = compute_daily_returns(df)
-# dates = pd.date_range('2013-01-01', '2013-12-31')
-# df = df.append(get_data(tickers, dates))
-# dates = pd.date_range('2014-01-01', '2014-12-31')
-# df = df.append(get_data(tickers, dates))
-# dates = pd.date_range('2015-01-01', '2015-12-31')
-# df = df.append(get_data(tickers, dates))
-# dates = pd.date_range('2016-01-01', '2016-12-31')
-# df = df.append(get_data(tickers, dates))
-
 # df.to_csv("data/stock_prices.c")
-
-with open('data/AAPL.json') as json_data:
-    data = json.load(json_data)
-
 
 def clean_date(date):
     mm = int(date[:2])
@@ -30,16 +13,34 @@ def clean_date(date):
     yy = int(date[4:])
     return datetime.date(yy, mm, dd)
 
-cleaned_data = {}
+def create_df_for_ticker(ticker):
+    tickers = [ticker]
+    dates = pd.date_range('2012-01-01', '2012-12-31')
+    df = get_data(tickers, dates)
+    df = compute_daily_returns(df)
+    dates = pd.date_range('2013-01-01', '2013-12-31')
+    df = df.append(get_data(tickers, dates))
+    dates = pd.date_range('2014-01-01', '2014-12-31')
+    df = df.append(get_data(tickers, dates))
+    dates = pd.date_range('2015-01-01', '2015-12-31')
+    df = df.append(get_data(tickers, dates))
+    dates = pd.date_range('2016-01-01', '2016-12-31')
+    df = df.append(get_data(tickers, dates))
 
-for date, sentiment in data.items():
-    cleaned_data[clean_date(date)] = sentiment
+    with open('data/{}.json'.format(ticker)) as json_data:
+        data = json.load(json_data)
 
+    cleaned_data = {}
 
-cd = pd.DataFrame.from_dict(cleaned_data, orient='index')
-cd.columns = ['Polarity', 'Subjectivity']
-# print(cd)
-cd = df.join(cd)
+    for date, sentiment in data.items():
+        cleaned_data[clean_date(date)] = sentiment
 
+    cd = pd.DataFrame.from_dict(cleaned_data, orient='index')
+    cd.columns = ['Polarity', 'Subjectivity']
+    cd = df.join(cd)
+    cd = cd.drop('SPY', 1)
 
+    return cd
+
+cd = create_df_for_ticker('AAPL')
 print(cd)
