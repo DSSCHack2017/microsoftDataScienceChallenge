de0bd9b228af72e0c24d029ba5815b033311ef61
Mujavid Bukhari <sirmuj@gmail.com>
2017-03-12 07:04:05 +0000

    Adjust axes

--- visualisations/index.js
+++ visualisations/index.js
@@ -30,8 +30,16 @@ const lineData = [
 	},
 	{
 		x: 100,
-		y: 60
-	}
+		y: 70
+	},
+	{
+		x: 120,
+		y: 80
+	},
+	{
+		x: 140,
+		y: 75
+	},
 ]
 
 const getDimensions = data => {
@@ -76,8 +84,8 @@ const drawAxis = () => {
 	   .attr("stroke", "white")
 	   .attr("stroke-width", STROKE_WIDTH)
 
-	let xDeciles = []
-	let yDeciles = []
+	const xDeciles = []
+	const yDeciles = []
 	for (let i = 1; i < 11; i++) {
 		xDeciles.push(DIMENSIONS.width * i/ 10)
 		yDeciles.push(DIMENSIONS.height * i/ 10)
@@ -88,36 +96,36 @@ const drawAxis = () => {
 		let y = transformPoint({x: 0, y: yDeciles[i]}).y
 		APP.append("text")
 			.attr("x", x - PADDING / 4)
-			.attr("y", DIMENSIONS.bottomLeft.y + PADDING / 2)
+			.attr("y", DIMENSIONS.bottomLeft.y + PADDING / 4)
 			.text(xDeciles[i])
 			.attr("fill", "grey")
+			.attr("font-size", "10px")
 		APP.append("text")
-			.attr("x", DIMENSIONS.bottomLeft.x - PADDING / 2)
+			.attr("x", DIMENSIONS.bottomLeft.x - PADDING / 4)
 			.attr("y", y)
 			.text(yDeciles[i])
 			.attr("fill", "grey")
+			.attr("font-size", "10px")
 		APP.append("rect")
 			.attr("x", DIMENSIONS.bottomLeft.x)
 			.attr("y", y)
 			.attr("width", DIMENSIONS.plotWidth)
 			.attr("height", DIMENSIONS.plotHeight / 10)
-			.attr("fill", "rgba(255,255,255,0.05)")
+			.attr("fill", "rgba(255,255,255,0.02)")
 		APP.append("path")
 			.attr("d", axisGenerator([{x: DIMENSIONS.bottomLeft.x, y: y}, {x: DIMENSIONS.bottomRight.x, y: y}]))
-			.attr("stroke", "rgba(255,255,255,0.2)")
+			.attr("stroke", "rgba(255,255,255,0.15)")
 			.attr("stroke-width", STROKE_WIDTH)
 	}
 
 	PATH_AREA = APP.append("g")
 
-	APP.append("text")
-	   .text("x-axis")
-	   .attr("x", WIDTH - PADDING)
-	   .attr("y", HEIGHT - PADDING)
-	APP.append("text")
-	   .text("y-axis")
-	   .attr("x", PADDING)
-	   .attr("y", PADDING)
+	const xAxis = APP.append("text").text("Time (years)")
+	const yAxis = APP.append("text").text("Change in Stock Price (%)")
+
+	xAxis.attr("x", PADDING + DIMENSIONS.plotWidth/2 - xAxis.node().getBoundingClientRect().width / 2)
+		 .attr("y", HEIGHT - PADDING / 4)
+	yAxis.attr("transform", `translate(${PADDING / 2},${PADDING + DIMENSIONS.plotHeight / 2 + yAxis.node().getBoundingClientRect().width / 2})rotate(-90)`)
 }
 
 const transformPoint = point => {
@@ -161,9 +169,9 @@ const setup = () => {
 	const viewSizes = getDimensions(lineData)
 
 	APP = d3.select("#root")
-	PADDING = 30
+	PADDING = 80
 	ASPECT_RATIO = 720 / 480
-	WIDTH = 720
+	WIDTH = 960
 	HEIGHT = WIDTH / ASPECT_RATIO
 	STROKE_WIDTH = 1
 
@@ -173,7 +181,7 @@ const setup = () => {
 		bottomLeft: {x:PADDING,y: HEIGHT - PADDING},
 
 		width: viewSizes.width.max - viewSizes.width.min,
-		height: viewSizes.height.max - viewSizes.height.min,
+		height: viewSizes.height.max - viewSizes.height.min + 10,
 
 		plotWidth: WIDTH - 2 * PADDING,
 		plotHeight: HEIGHT - 2 * PADDING
